8c18b88d302ce6b31eec600573ccf34b5c1363ef
Lorenzo Ruocco <lorenzoruocco@MacBook-Pro-de-Lorenzo.local>
2023-06-12 12:29:24 +0200

    create auth.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'yo' });
+  res.send("express is working")
 });
 
 module.exports = router;
